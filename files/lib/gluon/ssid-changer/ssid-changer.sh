08723a3162a6a1a0e118406c8927276e95d8fceb
fuzzle <jens@viisauksena.de>
2016-09-28 05:51:22 +0200

    bugfix ssid changer

--- files/lib/gluon/ssid-changer/ssid-changer.sh
+++ files/lib/gluon/ssid-changer/ssid-changer.sh
@@ -11,11 +11,13 @@ if [ $check -eq 0 ] ; then
         if [ "$(uci get wireless.client_radio0.ssid)" == "$offi" ] ; then echo "$0 - still on $offi"|logger ; exit 0 ; fi
         echo "$0 change ssid to $offi" | logger
         uci set wireless.client_radio0.ssid="$offi"
+        sed -i s/^ssid=$default/ssid=$offi/ /var/run/hostapd-phy0.conf
         killall -HUP hostapd
 fi
 if [ $check -gt 0 ] ; then
         if [ "$(uci get wireless.client_radio0.ssid)" == "$default" ] ; then echo "$0 - still on $default"|logger ; exit 0 ; fi
         echo "$0 change ssid to $default"| logger
         uci set wireless.client_radio0.ssid=freiburg.freifunk.net
+	sed -i s/^ssid=$offi/ssid=$default/ /var/run/hostapd-phy0.conf
         killall -HUP hostapd
 fi
