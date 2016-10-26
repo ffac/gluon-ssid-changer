5949071927d53b64a6c9d89e3e5b1fd3c304e541
fuzzle <jens@viisauksena.de>
2016-10-26 21:24:21 +0200

    reduce logging if nothing worthy to report

--- files/lib/gluon/ssid-changer/ssid-changer.sh
+++ files/lib/gluon/ssid-changer/ssid-changer.sh
@@ -8,14 +8,14 @@ default="freiburg.freifunk.net"
 offi=$offline$name
 
 if [ $check -eq 0 ] ; then
-        if [ "$(uci get wireless.client_radio0.ssid)" == "$offi" ] ; then echo "$0 - still on $offi"|logger ; exit 0 ; fi
+        if [ "$(uci get wireless.client_radio0.ssid)" == "$offi" ] ; then echo "$0 - still on $offi" ; exit 0 ; fi
         echo "$0 change ssid to $offi" | logger
         uci set wireless.client_radio0.ssid="$offi"
         sed -i s/^ssid=$default/ssid=$offi/ /var/run/hostapd-phy0.conf
         killall -HUP hostapd
 fi
 if [ $check -gt 0 ] ; then
-        if [ "$(uci get wireless.client_radio0.ssid)" == "$default" ] ; then echo "$0 - still on $default"|logger ; exit 0 ; fi
+        if [ "$(uci get wireless.client_radio0.ssid)" == "$default" ] ; then echo "$0 - still on $default" ; exit 0 ; fi
         echo "$0 change ssid to $default"| logger
         uci set wireless.client_radio0.ssid=freiburg.freifunk.net
 	sed -i s/^ssid=$offi/ssid=$default/ /var/run/hostapd-phy0.conf
