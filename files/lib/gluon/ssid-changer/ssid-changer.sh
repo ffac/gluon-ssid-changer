1d35be8f95f0e6ba0b8df83a2f603c2a1a218725
rubo77 <github@r.z11.de>
2016-11-11 03:12:19 +0100

    take more of the node name into the offline SSID
    And code cleanup and comments

--- files/lib/gluon/ssid-changer/ssid-changer.sh
+++ files/lib/gluon/ssid-changer/ssid-changer.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 
-# maximum simplyfied, no more ttvn rating
-check=$(batctl gwl -H|grep -v "gateways in range"|wc -l)
-name=$(nodename status|tail -c 21)
-offline="FF_OFFLINE_"
-default="FREIFUNK"
-offi=$offline$name
+# at first some Definitions:
+ONLINE_SSID=$(uci get wireless.client_radio0.ssid -q)
+: ${ONLINE_SSID:="FREIFUNK"} # if for whatever reason ONLINE_SSID is NULL
+OFFLINE_PREFIX='FF_OFFLINE_' # use something short to leave space for the nodename
 
-if [ $check -eq 0 ] ; then
-        if [ "$(uci get wireless.client_radio0.ssid)" == "$offi" ] ; then echo "$0 - still on $offi" ; exit 0 ; fi
-        echo "$0 change ssid to $offi" | logger
-        uci set wireless.client_radio0.ssid="$offi"
-        sed -i s/^ssid=$default/ssid=$offi/ /var/run/hostapd-phy0.conf
-        killall -HUP hostapd
+# Generate an Offline SSID with the first and last part of the nodename to allow owner to recognise wich node is down
+NODENAME=`uname -n`
+if [ ${#NODENAME} -gt $((30 - ${#OFFLINE_PREFIX})) ] ; then # 32 would be possible as well
+	HALF=$(( (28 - ${#OFFLINE_PREFIX} ) / 2 )) # calculate the length of the first part of the node identifier in the offline-ssid
+	SKIP=$(( ${#NODENAME} - $HALF )) # jump to this charakter for the last part of the name
+	OFFLINE_SSID=$OFFLINE_PREFIX${NODENAME:0:$HALF}...${NODENAME:$SKIP:${#NODENAME}} # use the first and last part of the nodename for nodes with long name
+else
+	OFFLINE_SSID="$OFFLINE_PREFIX$NODENAME" # great! we are able to use the full nodename in the offline ssid
 fi
-if [ $check -gt 0 ] ; then
-        if [ "$(uci get wireless.client_radio0.ssid)" == "$default" ] ; then echo "$0 - still on $default" ; exit 0 ; fi
-        echo "$0 change ssid to $default"| logger
-        uci set wireless.client_radio0.ssid="$default"
-	sed -i s/^ssid=$offi/ssid=$default/ /var/run/hostapd-phy0.conf
-        killall -HUP hostapd
+# maximum simplyfied, no more ttvn rating
+CHECK=$(batctl gwl -H|grep -v "gateways in range"|wc -l)
+
+if [ $CHECK -eq 0 ] ; then
+  if [ "$(uci get wireless.client_radio0.ssid)" == "$OFFLINE_SSID" ] ; then echo "$0 - still on $OFFLINE_SSID" ; exit 0 ; fi
+  echo "$0 change ssid to $OFFLINE_SSID" | logger
+  uci set wireless.client_radio0.ssid="$OFFLINE_SSID"
+  sed -i s/^ssid=$ONLINE_SSID/ssid=$OFFLINE_SSID/ /var/run/hostapd-phy0.conf
+  killall -HUP hostapd
+fi
+if [ $CHECK -gt 0 ] ; then
+  if [ "$(uci get wireless.client_radio0.ssid)" == "$ONLINE_SSID" ] ; then echo "$0 - still on $ONLINE_SSID" ; exit 0 ; fi
+  echo "$0 change ssid to $ONLINE_SSID"| logger
+  uci set wireless.client_radio0.ssid="$ONLINE_SSID"
+  sed -i s/^ssid=$OFFLINE_SSID/ssid=$ONLINE_SSID/ /var/run/hostapd-phy0.conf
+  killall -HUP hostapd
 fi
